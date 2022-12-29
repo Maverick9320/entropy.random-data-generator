64eeb0be5b91a08e86739dc81e7699fc6c6387fa
Maverick9320 <99112649+Maverick9320@users.noreply.github.com>
2022-12-29 18:36:39 +0200

    Update and rename functions.py to example.py

--- example.py
+++ example.py
@@ -5,4 +5,7 @@ x.int_gen(column='Integer Column')
 x.str_gen(column='String Column')
 x.bool_gen(column='Boolean Column', trinary='Unknown')
 
+x = complex_generator(x)
+x.date_gen(column='Date Column')
+
 dataframe = x.output()
